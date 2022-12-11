8494fa26b231d2dae51855ad3b12e2c7f547ce7f
Shaked Eyal <shaked.eyal@protonmail.com>
2022-12-11 19:11:13 +0200

    Examples for creating flux

--- src/main/java/org/ractor/Main.java
+++ src/main/java/org/ractor/Main.java
@@ -1,8 +1,17 @@
 package org.ractor;
 
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Flux<String> seq1 = Flux.just("foo", "bar", "footbar"); // Use enumerate to create flux
+
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar"); // Create a collection
+        Flux<String> seq2 = Flux.fromIterable(iterable); // use the collection to create a flux
     }
 }
