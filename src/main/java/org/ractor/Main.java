bafb0a1eaf8b4e9f9fc0b1bc1808a74ff090b5b6
Shaked Eyal <shaked.eyal@protonmail.com>
2022-12-11 23:25:46 +0200

    Add more examples

--- src/main/java/org/ractor/Main.java
+++ src/main/java/org/ractor/Main.java
@@ -1,6 +1,7 @@
 package org.ractor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,5 +14,9 @@ public class Main {
 
         List<String> iterable = Arrays.asList("foo", "bar", "foobar"); // Create a collection
         Flux<String> seq2 = Flux.fromIterable(iterable); // use the collection to create a flux
+
+        Mono<String> noData = Mono.empty(); // creates an empty mono with no data to pass
+        Mono<String> data = Mono.just("foo"); // creates a single element in the stream.
+        Flux<Integer> numbersFromFiveToSeven = Flux.range(5, 3); // first parameter is start, second in number of items
     }
 }
