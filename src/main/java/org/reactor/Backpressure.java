3ae38c986d86a17e920049ac12d33bdcb0e288dc
Shaked Eyal <shaked.eyal@protonmail.com>
2023-01-07 21:37:27 +0200

    Add example for backpressure

---
+++ src/main/java/org/reactor/Backpressure.java
@@ -0,0 +1,24 @@
+package org.reactor;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class Backpressure {
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+            .doOnRequest(r -> System.out.println("request of " + r))
+            .subscribe(new BaseSubscriber<Integer>() {
+                @Override
+                protected void hookOnSubscribe(Subscription subscription) {
+                    request(1);
+                }
+
+                @Override
+                protected void hookOnNext(Integer value) {
+                    System.out.println("Cancelling after having received " + value);
+                    cancel();
+                }
+            });
+    }
+}
